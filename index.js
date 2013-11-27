a9f020ed64fd153f17d279f04d56c899611ca9f6
Gergely Nemeth <gergelyke@Gergelys-MacBook-Pro.local>
2013-11-27 08:27:02 +0100

    only xx days added

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
 var ac = require('ansi-canvas');
 
-var canvas = ac();
-var context = canvas.getContext('2d');
+var canvas = ac(),
+    i = 0,
+    context;
 
-// draw a purple background
-context.fillStyle = 'purple';
-context.fillRect(0, 0, canvas.width, canvas.height);
+function draw () {
+    context = canvas.getContext('2d');
 
-// write some text
-context.fillStyle    = '#00f';
-context.font         = 'italic 15px sans-serif';
-context.textBaseline = 'top';
-context.fillText  ('Hello world!', 1, 1);
-context.font         = 'bold 20px sans-serif';
-context.strokeText('Hello world!', 1, 21);
+// draw a white background
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, canvas.width, canvas.height);
 
-// IMPORTANT!!!
-// call `canvas.render()` when you're ready to flush the canvas to the terminal
-canvas.render();
+// write Only XX days
+    context.fillStyle = 'red';
+    context.font = 'italic ' + ((8 + (i%4) * 0.5) + 'px') + ' sans-serif';
+    context.textBaseline = 'bottom';
+    context.fillText('Only ' + daysBetween(new  Date(), new Date(2013,11,25)) + ' days till XMAS!', 1, 10);
+}
+
+function render() {
+    draw();
+    var termCtx = canvas.getContext('2d');
+    termCtx.clearRect(0, 0, context.width, context.height);
+    i += 1;
+    termCtx.drawImage(canvas, 0, 0, canvas.width, canvas.height);
+    canvas.render();
+}
+
+function daysBetween(first, second) {
+
+    // Copy date parts of the timestamps, discarding the time parts.
+    var one = new Date(first.getFullYear(), first.getMonth(), first.getDate());
+    var two = new Date(second.getFullYear(), second.getMonth(), second.getDate());
+
+    // Do the math.
+    var millisecondsPerDay = 1000 * 60 * 60 * 24;
+    var millisBetween = two.getTime() - one.getTime();
+    var days = millisBetween / millisecondsPerDay;
+
+    // Round down.
+    return Math.floor(days);
+}
+
+
+setInterval(function(){
+    render();
+}, 100);
